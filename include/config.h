71b7afb7ea9bdbdbcd1e2b77402856fc76697a69
Code1deep <az.bahya@gmail.com>
2026-04-24 19:40:48 -0400

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi configuration
+const char* WIFI_SSID = "votre_reseau";
+const char* WIFI_PASSWORD = "votre_mot_de_passe";
+
+// MQTT configuration
+const char* MQTT_BROKER = "broker.emqx.io";
+const char* MQTT_TOPIC = "productique/bdi/sensors";
+
+#endif
